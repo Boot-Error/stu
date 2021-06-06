04d68d224cb880c1905c611e34ed3e7ee93ae6a6
Boot-Error <booterror99@gmail.com>
2021-06-06 12:29:31 +0530

    bugfix: weeklty trend scraper script function export

--- scripts/v2hconvert.sh
+++ scripts/v2hconvert.sh
@@ -2,16 +2,8 @@
 
 set -e
 
-INPUT_DIR=$1
+input_file=$1
 OUTPUT_DIR=$2
 
-function convertToLandspaceWithBlur() {
-  input_file=$1
-  output_file=$OUTPUT_DIR/$(basename $input_file)
-  ffmpeg -i {} -vf 'split[original][copy];[copy]scale=ih*16/9:-1,crop=h=iw*9/16,gblur=sigma=20[blurred];[blurred][original]overlay=(main_w-overlay_w)/2:(main_h-overlay_h)/2' $output_file 
-
-}
-
-export -f convertToLandspaceWithBlur
-
-ls $INPUT_DIR/*.mp4 | xargs -I {} convertToLandspaceWithBlur {}
+output_file=$OUTPUT_DIR/$(basename $input_file)
+ffmpeg -i "$input_file" -vf 'split[original][copy];[copy]scale=ih*16/9:-1,crop=h=iw*9/16,gblur=sigma=20[blurred];[blurred][original]overlay=(main_w-overlay_w)/2:(main_h-overlay_h)/2' "$output_file" 
